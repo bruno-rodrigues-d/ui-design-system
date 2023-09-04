e16aa99c5cf3bc8d7476cf216ac55f0d9d6d2d57
bruno-rodrigues-d <brunorodrigues@gec.inatel.br>
2023-09-04 16:51:10 -0300

    feat: config Changesets on npm

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Heading, HeadingProps } from '@ignite-ui/react'
+import { Heading, HeadingProps } from '@bruno-rd-ignite-ui/react'
 
 export default {
   title: 'Typography/Heading',
